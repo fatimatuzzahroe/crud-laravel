805a88b4050791ac082fe030dc63bb8d544c883c
fatimatuzzahroe <fatimatuz.zahroe@gmail.com>
2023-02-13 19:31:21 +0700

    form tambah cabang

--- resources/views/master/cabang/tambah.blade.php
+++ resources/views/master/cabang/tambah.blade.php
@@ -1,5 +1,29 @@
 @extends('master')
 @section('konten-master')
 
-form untuk menambah master cabang
+<form class="mb-5">
+    <div class="row">
+        <div class="col-3">
+            <div class="mb-5">
+                <label class="form-label">Kode Cabang</label>
+                <input type="text" class="form-control">
+            </div>
+        </div>
+        <div class="col-9">
+            <div class="mb-5">
+                <label class="form-label">Nama Cabang</label>
+                <input type="text" class="form-control">
+            </div>
+        </div>
+    </div>
+    <div class="mb-5">
+        <label class="form-label">Nomor Telepon</label>
+        <input type="text" class="form-control">
+    </div>
+    <div class="mb-5">
+        <label class="form-label">Alamat</label>
+        <textarea name="" id="" rows="5" class="form-control"></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
 @endsection
